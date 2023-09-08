6fcd275a8e5c56bcc578a5654098e3255f7b6482
OFFICEJAMES\james <jamesoger@hotmail.com>
2023-09-08 18:31:22 -0400

    petit changement james (h1 index programmation)

--- laravel/resources/views/programmation/index.blade.php
+++ laravel/resources/views/programmation/index.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
     <h1>Programmation!</h1>
     @foreach ($programmation as $prog)
+    <h1>{{ $prog->date }}</h1>
     <p>
         @foreach ($prog->artistes as $artiste)
             {{ $artiste->nom_scene }}
