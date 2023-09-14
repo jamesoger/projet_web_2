4d58fc5147fe565c943ad9438f056c9264b8ce4d
CSTJ\0479854 <geoffpas@gmail.com>
2023-09-14 11:51:09 -0400

    Html du header de l'accueil

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -11,22 +11,46 @@
     </nav>
 
     <header class="acc-header">
+        <ul class="acc-media">
+            <li class="acc-media-fb">
+                <img src="{{ asset('icones/facebook.png') }}" alt="" class="acc-fb">
+            </li>
+            <li class="acc-media-fb">
+                <img src="{{ asset('icones/twitter.png') }}" alt="" class="acc-fb">
+            </li>
+            <li class="acc-media-fb">
+                <img src="{{ asset('icones/instagram.png') }}" alt="" class="acc-fb">
+            </li>
+        </ul>
+
+        <div class="acc-centre">
+            <p class="acc-date">
+                9.08 au 11.08
+            </p>
+            <div class="acc-logo">
+                <img src="{{ asset('logos/centre_color_blanc.png')}}" alt="" class="logo">
+            </div>
+            <div class="acc-fleche">
+                <div class="cercle">
+                    <img src="{{ asset('icones/fleche.svg') }}" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="acc-btn-menu">
+            <div class="acc-btn">
+                <img src="{{ asset('icones/menu_9_points.png') }}" alt="">
+            </div>
+            <p class="acc-menu">
+                menu
+            </p>
+        </div>
+
         <div class="video-background">
             <video autoplay loop muted poster="{{ asset('images/cadre_header.jpg') }}">
                 <source src="{{ asset('videos/cadre_header.mp4') }}" type="video/mp4">
                 Votre navigateur ne prend pas en charge la lecture de vidéos.
             </video>
-        </div>
-        <div class="acc-media">
-            <div class="acc-media-fb">
-                <img src="{{ asset('icones/facebook.png') }}" alt="" class="acc-fb">
-            </div>
-            <div class="acc-media-fb">
-                <img src="{{ asset('icones/twitter.png') }}" alt="" class="acc-fb">
-            </div>
-            <div class="acc-media-fb">
-                <img src="{{ asset('icones/instagram.png') }}" alt="" class="acc-fb">
-            </div>
         </div>
     </header>
 
