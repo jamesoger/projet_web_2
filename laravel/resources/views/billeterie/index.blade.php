22899c6a3bdc3df850f949b7ea71a2e4b3083b2e
OFFICEJAMES\james <jamesoger@hotmail.com>
2023-09-16 19:07:40 -0400

    un peu de css page billeterie version 2 probleme branche

--- laravel/resources/views/billeterie/index.blade.php
+++ laravel/resources/views/billeterie/index.blade.php
@@ -1,16 +1,25 @@
 <x-layout>
-    <h1>Billeterie!</h1>
-    <h1>@foreach ($forfaits as $forfait )
-        <p>{{ $forfait->nom}}</p>
-        <p>{{ $forfait->prix}}</p>
+    <div class="bill">
+        <div class="bill_forfaits">
+            @foreach ($forfaits as $key => $forfait)
+                <div class="bill_boite
+                    {{ $forfait->id === 1 ? 'boite_1' : '' }}
+                    {{$forfait->id === 2 ? 'boite_gauche' : '' }}
+                    {{ $forfait->id === 3 ? 'boite_droite' : '' }}"
+                    style="background-image: url('{{ $forfait->image }}'); background-size: cover;">
+                    <div class="bill_nom">
+                        <p>{{ $forfait->nom }}</p>
+                    </div>
 
-        @if (auth()->check())
-    {{-- L'utilisateur est déjà connecté, redirigez vers la page de panier --}}
-    <a href="{{ route('user.panier',['forfait_id' => $forfait->id]) }}">Réservez</a>
-@else
-    {{-- L'utilisateur n'est pas connecté, affichez le lien "Réserver" avec le paramètre 'from_page' --}}
-    <a href="{{ route('user_connexion.create', ['forfait_id' => $forfait->id]) }}">Réservez</a>
-@endif
-    @endforeach</h1>
-
+                    @if (auth()->check())
+                        {{-- L'utilisateur est déjà connecté, redirigez vers la page de panier --}}
+                        <a href="{{ route('user.panier', ['forfait_id' => $forfait->id]) }}">Réservez</a>
+                    @else
+                        {{-- L'utilisateur n'est pas connecté, affichez le lien "Réserver" avec le paramètre 'from_page' --}}
+                        <a href="{{ route('user_connexion.create', ['forfait_id' => $forfait->id]) }}">Réservez</a>
+                    @endif
+                </div>
+            @endforeach
+        </div>
+    </div>
 </x-layout>
