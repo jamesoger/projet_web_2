5622158d0dd2d3063e98eb432bd49a0757399285
OFFICEJAMES\james <jamesoger@hotmail.com>
2023-09-16 12:24:24 -0400

    travail sur forfaits/session

--- laravel/resources/views/actualites/index.blade.php
+++ laravel/resources/views/actualites/index.blade.php
@@ -3,6 +3,7 @@
 
         <h1>Actualités!</h1>
         @foreach ($actualites  as $actualite)
+            <h2>{{$actualite->date}}</h2>
             <h2>{{$actualite->titre}}</h2>
             <p>{{$actualite->details}}</p>
             <img src="{{ $actualite->image }}" width="800px" alt="">
