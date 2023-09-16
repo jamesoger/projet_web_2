458646dd01853cc2d85540de6a15585c1e10a983
OFFICEJAMES\james <jamesoger@hotmail.com>
2023-09-16 18:37:55 -0400

    un poeu de css page billeterie

--- laravel/database/migrations/2023_09_08_194116_create_forfaits_table.php
+++ laravel/database/migrations/2023_09_08_194116_create_forfaits_table.php
@@ -17,6 +17,7 @@ class CreateForfaitsTable extends Migration
             $table->id();
             $table->string('nom');
             $table->decimal('prix');
+            $table->string('image');
             $table->timestamps();
         });
     }
