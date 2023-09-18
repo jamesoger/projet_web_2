fe1102d112814601c82c17c8035b4222e3e0832f
DESKTOP-4SG7KTO\maxim <maxime-n-f@hotmail.fr>
2023-09-18 15:20:31 -0400

    ajout du nav et de à propos

--- laravel/resources/views/a_propos/index.blade.php
+++ laravel/resources/views/a_propos/index.blade.php
@@ -1,4 +1,43 @@
 <x-layout titre="À propos">
     <x-nav />
-    <h1>A propos!</h1>
+    <main class="a-propos">
+        <h1>A proposde FestX</h1>
+        <div class="texte-apropos">
+            <p>Bienvenue dans l'univers époustouflant de FestX, le festival où la musique électronique fusionne avec la
+                technologie pour créer une expérience sensorielle unique en son genre. Au cœur de notre festival, vous
+                découvrirez une convergence inédite entre les rythmes envoûtants de la musique techno et électro et
+                l'art
+                visuel futuriste des spectacles de drones illuminés.
+            </p>
+            <p>FestX est bien plus qu'un simple festival de musique ; c'est une célébration de l'innovation, de la
+                créativité et de la communion entre l'homme et la machine. Nous avons réuni des artistes de renommée
+                mondiale, des DJ talentueux et des concepteurs de spectacles de drones éblouissants pour vous offrir une
+                expérience immersive hors du commun.
+            </p>
+            <p>Au sein de notre espace enchanté, la musique pulse au rythme de la vie, vous transportant dans un voyage
+                sonore envoûtant. Les beats puissants, les mélodies hypnotiques et les sets électroniques de pointe
+                s'entremêleront pour créer des moments de pure euphorie, vous entraînant dans une danse effrénée
+                jusqu'au
+                bout de la nuit.
+            </p>
+            <p>Mais FestX, c'est également l'exploration des possibilités infinies de la technologie. Imaginez des
+                essaims
+                de drones illuminés, dansant dans le ciel nocturne comme des étoiles artificielles, créant des tableaux
+                visuels extraordinaires qui captiveront vos sens. Ces spectacles de drones vous transporteront dans un
+                monde
+                de rêve, où la lumière et la musique se marient pour créer une expérience multisensorielle inoubliable.
+            </p>
+            <p>Notre festival est bien plus qu'un simple événement musical, c'est une célébration de l'avenir, de la
+                créativité et de la collaboration entre l'homme et la machine. Alors préparez-vous à vivre une
+                expérience
+                transcendantale au Festival FestX, où la musique, la technologie et l'art se rejoignent pour créer des
+                souvenirs qui dureront toute une vie. Rejoignez-nous pour une nuit de musique électronique et de
+                spectacles
+                de drones qui repousseront les limites de l'imagination.</p>
+            <p>Réservez vos billets dès maintenant et préparez-vous à plonger dans l'univers captivant de FestX, là où
+                la
+                magie opère et les étoiles s'allument, tant dans le ciel que dans votre cœur.
+            </p>
+        </div>
+    </main>
 </x-layout>
