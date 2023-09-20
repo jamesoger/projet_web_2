78c01af69c05eed9217c6f02d9f89068e95ccd81
APINDIX_PC\geoff <geoffpas@gmail.com>
2023-09-20 09:52:12 -0400

    CSS actu.edit

--- laravel/resources/views/auth/user/connexion/create.blade.php
+++ laravel/resources/views/auth/user/connexion/create.blade.php
@@ -1,12 +1,15 @@
 <x-layout titre="Connexion">
     <x-nav />
+
     <div class="connexion_user">
         <h1>connexion</h1>
+
         @if (request()->route()->hasParameter('forfait_id'))
             <div>
                 <h2>Pour réserver ce forfait, il faut vous connecter</h1>
             </div>
         @endif
+
         @if (session('email'))
             <p>{{ session('email') }}</p>
         @endif
@@ -14,31 +17,27 @@
         <div class="connexion_user_form">
             <form action="{{ route('user_connexion.authentifier') }}" method="POST">
                 @csrf
+
                 <div>
                     <label for="email">Courriel</label>
                     <x-forms.erreur champ="email" />
 
                     <div>
-                        <input id="email" name="email" type="email" autocomplete="email"
-                            value="{{ old('email') }}">
+                        <input id="email" name="email" type="email" autocomplete="email" value="{{ old('email') }}">
                     </div>
                 </div>
+
                 <div>
-                    <div>
-                        <label for="password">
-                            Mot de passe
-                        </label>
-                    </div>
+                    <label for="password">Mot de passe</label>
                     <x-forms.erreur champ="password" />
 
                     <div>
                         <input id="password" name="password" type="password" autocomplete="current-password">
                     </div>
                 </div>
+
                 <div>
-                    <button type="submit">
-                        Connectez-vous!
-                    </button>
+                    <button type="submit">Connectez-vous !</button>
                 </div>
             </form>
         </div>
