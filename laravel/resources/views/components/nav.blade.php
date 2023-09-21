08b1e4af5c65660e7cf2c91ceea664ab39c9d191
CSTJ\0479854 <geoffpas@gmail.com>
2023-09-21 10:00:40 -0400

    Pofinage barre de NAV (via le main)

--- laravel/resources/views/components/nav.blade.php
+++ laravel/resources/views/components/nav.blade.php
@@ -1,9 +1,11 @@
 <nav class="nav-menu">
-    <img src="{{ asset('logos/plat_gold_blanc.png') }}" alt="">
-    {{-- <a href="{{ route('actualites.index') }}">Actualités</a>
-    <a href="{{ route('billetterie.index') }}">Billetterie</a>
-    <a href="{{ route('programmation.index') }}">Programmation</a>
-    <a href="{{ route('info.index') }}">À propos</a>
-    <a href="{{ route('user_connexion.create') }}">Mon compte</a>
-    <a href="{{ route('admin_connexion.login') }}">admin</a> --}}
+    <a href="{{ route('accueil') }}">
+        <img src="{{ asset('logos/plat_gold_blanc.png') }}" alt="">
+    </a>
+    <a class="nav-a" href="{{ route('actualites.index') }}">Actualités</a>
+    <a class="nav-a" href="{{ route('billetterie.index') }}">Billetterie</a>
+    <a class="nav-a" href="{{ route('programmation.index') }}">Programmation</a>
+    <a class="nav-a" href="{{ route('info.index') }}">À propos</a>
+    <a class="nav-a" href="{{ route('user_connexion.create') }}">Mon compte</a>
+    <a class="nav-a" href="{{ route('admin_connexion.login') }}">admin</a>
 </nav>
