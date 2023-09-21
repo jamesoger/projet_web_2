ecbedb7f5fa9b8509f7a0d0139b5fe8a544f43e8
CSTJ\6212221 <jamesoger@hotmail.com>
2023-09-21 10:42:54 -0400

    psuh test

--- laravel/resources/views/programmation/spectacle/edit.blade.php
+++ laravel/resources/views/programmation/spectacle/edit.blade.php
@@ -1,52 +1,55 @@
 <x-layout titre="Modification d'un spectacle">
     <x-nav />
-    @if (session('error'))
-        <p style="color: red">{{ session('error') }}</p>
-    @endif
-    <h1>{{ $spectacle->nom }} </h1>
-    <form action="{{ route('programmation.spectacle.update') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <input type="hidden" name="id" value="{{ $spectacle->id }}">
-        <div>
+    <div class="spectacle_edit">
+        @if (session('error'))
+            <p style="color: red">{{ session('error') }}</p>
+        @endif
+        <h1>{{ $spectacle->nom }} </h1>
+        <form action="{{ route('programmation.spectacle.update') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <input type="hidden" name="id" value="{{ $spectacle->id }}">
             <div>
-                <label for="nom">Nom</label>
-                @error('nom')
-                    <p>{{ $message }}</p>
-                @enderror
-                <input type="text" name="nom" value="{{ $spectacle->nom }}">
-            </div>
-            <div>
-                <label for="heure">Heure de la représentation</label>
-                @error('heure')
-                    <p>{{ $message }}</p>
-                @enderror
-                <input type="time" name="heure" value="{{ $spectacle->heure }}">
-            </div>
-            <div>
-                <label for="image">Image</label>
-                @error('image')
-                    <p>{{ $message }}</p>
-                @enderror
-                <input type="file" name="image" accept="image/*">
-                <input type="hidden" name="image_spectacle"value="{{$spectacle->image}}">
-            </div>
-            <div>
-                <label for="date">Date de la représentation</label>
-                @error('date')
-                    <p>{{ $message }}</p>
-                @enderror
-                <select name="date">
-                    @foreach ($programmations as $programmation)
-                        <option value="{{ $programmation->id }}"
-                            {{ $spectacle->programmations->contains('id', $programmation->id) ? 'selected' : '' }}>
-                            {{ $programmation->date }}
-                        </option>
-                    @endforeach
-                </select>
+                <div>
+                    <label for="nom">Nom</label>
+                    @error('nom')
+                        <p>{{ $message }}</p>
+                    @enderror
+                    <input type="text" name="nom" value="{{ $spectacle->nom }}">
+                </div>
+                <div>
+                    <label for="heure">Heure de la représentation</label>
+                    @error('heure')
+                        <p>{{ $message }}</p>
+                    @enderror
+                    <input type="time" name="heure" value="{{ $spectacle->heure }}">
+                </div>
+                <div>
+                    <label for="image">Image</label>
+                    @error('image')
+                        <p>{{ $message }}</p>
+                    @enderror
+                    <input type="file" name="image" accept="image/*">
+                    <input type="hidden" name="image_spectacle"value="{{ $spectacle->image }}">
+                </div>
+                <div>
+                    <label for="date">Date de la représentation</label>
+                    @error('date')
+                        <p>{{ $message }}</p>
+                    @enderror
+                    <select name="date">
+                        @foreach ($programmations as $programmation)
+                            <option value="{{ $programmation->id }}"
+                                {{ $spectacle->programmations->contains('id', $programmation->id) ? 'selected' : '' }}>
+                                {{ $programmation->date }}
+                            </option>
+                        @endforeach
+                    </select>
 
+                </div>
             </div>
-        </div>
-        <input type="submit" value="Envoyer">
+            <input type="submit" value="Envoyer">
 
-    </form>
+        </form>
+    </div>
+
 </x-layout>
