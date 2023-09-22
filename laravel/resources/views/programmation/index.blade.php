20f77b584346e716edf05f6dbfcf890daa89464c
OFFICEJAMES\james <jamesoger@hotmail.com>
2023-09-22 17:44:38 -0400

    css page programmation, ça avance!

--- laravel/resources/views/programmation/index.blade.php
+++ laravel/resources/views/programmation/index.blade.php
@@ -1,8 +1,10 @@
 <x-layout titre="Programmation">
     <x-nav />
     <div class="prog">
-        @foreach ($programmation as $prog)
+        @foreach ($programmation as $key => $prog)
+        <div class="date {{ $key % 3 == 0 ? 'date-gauche' : ($key % 3 == 1 ? 'date-centre' : 'date-droite') }}">
             <h1>{{ Carbon\Carbon::parse($prog->date)->translatedFormat('d F Y') }}</h1>
+        </div>
             <div class="prog_bubbles">
                 @php
                     $artistImageClasses = ['image-haut', 'image-milieu', 'image-bas'];
@@ -14,23 +16,25 @@
                 @foreach ($prog->artistes as $artiste)
                     <div class="image_bubbles">
                         <img class="{{ $artistImageClasses[$artistClassIndex] }}" src="{{ $artiste->image }}" alt="">
-                        {{-- <span> {{ $artiste->nom_scene }}</span>
-                        <span> {{ $artiste->heure_show }}</span> --}}
-                    </div>
+                        <span class="{{ $artistImageClasses[$artistClassIndex] }}"> {{ $artiste->nom_scene }}</span>
+                        <span  id="nom" class="{{ $artistImageClasses[$artistClassIndex] }}"> {{ $artiste->heure_show }}</span>
+
                     @php
                         $artistClassIndex = ($artistClassIndex + 1) % count($artistImageClasses);
                     @endphp
+                    </div>
                 @endforeach
 
                 @foreach ($prog->spectacles as $spectacle)
                     <div class="image_bubbles">
                         <img class="{{ $spectacleImageClasses[$spectacleClassIndex] }}" src="{{ $spectacle->image }}" alt="">
-                        {{-- <span>{{ $spectacle->nom }}</span>
-                        <span> {{ $spectacle->heure }}</span> --}}
-                    </div>
+                        <span class="{{ $spectacleImageClasses[$spectacleClassIndex] }}" >{{ $spectacle->nom }}</span>
+                        <span id="nom" class="{{ $spectacleImageClasses[$spectacleClassIndex] }}"> {{ $spectacle->heure }}</span>
+
                     @php
                         $spectacleClassIndex = ($spectacleClassIndex + 1) % count($spectacleImageClasses);
                     @endphp
+                     </div>
                 @endforeach
             </div>
         @endforeach
