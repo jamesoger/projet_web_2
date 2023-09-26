e0c133f0674db16bcb1e4bbfefe31c2c4786d127
APINDIX_PC\geoff <geoffpas@gmail.com>
2023-09-26 09:27:04 -0400

    FORM : css user-edit

--- laravel/resources/views/user/edit.blade.php
+++ laravel/resources/views/user/edit.blade.php
@@ -1,26 +1,28 @@
 <x-layout titre="Modification de : {{ $user->prenom }}">
     <x-nav />
-    <div class="user_edit">
-        <h1>Modification de {{ $user->nom_complet }}</h1>
-        <div class="user_modif_form">
+    <div class="user-edit">
+        <h1>Modification de
+            <br><span>{{ $user->nom_complet }}</span>
+        </h1>
+        <div class="user-edit-form">
             <form action="{{ route('user.update', ['id' => $user->id]) }}" method="POST">
                 @csrf
-                <div class="form-group">
+                <div class="user-edit-input">
                     <label for="prenom">Prénom</label>
                     <x-forms.erreur champ="prenom" />
                     <input type="text" name="prenom" id="prenom" value="{{ $user->prenom }}">
                 </div>
-                <div class="form-group">
+                <div class="user-edit-input">
                     <label for="nom">Nom</label>
                     <x-forms.erreur champ="nom" />
                     <input type="text" name="nom" id="nom" value="{{ $user->nom }}">
                 </div>
-                <div class="form-group">
+                <div class="user-edit-input">
                     <label for="email">Adresse e-mail</label>
                     <x-forms.erreur champ="email" />
                     <input type="email" name="email" id="email" value="{{ $user->email }}">
                 </div>
-                <div class="form-group">
+                <div class="user-edit-input">
                     <button type="submit">Enregistrer les modifications</button>
                 </div>
         </div>
