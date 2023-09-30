ed0ca6030e8bb325e4757289261c74b9d3052246
APINDIX_PC\geoff <geoffpas@gmail.com>
2023-09-30 14:47:10 -0400

    amelioration css

--- laravel/resources/views/billetterie/index.blade.php
+++ laravel/resources/views/billetterie/index.blade.php
@@ -1,9 +1,11 @@
 <x-layout titre="Enregistrement">
     <x-nav />
+
     <div class="confirmation-message">
         {{-- message de confirmation --}}
         <x-message />
     </div>
+
     <div class="bill">
         <div class="bill_forfaits">
             @foreach ($forfaits as $key => $forfait)
@@ -16,15 +18,22 @@
                     <div class="bill_nom">
                         <p>{{ $forfait->nom }}</p>
                     </div>
-                    @if (auth()->check())
-                        <a href="{{ route('user.panier', ['forfait_id' => $forfait->id]) }}">Réservez</a>
-                    @else
-                        <a href="{{ route('user_connexion.create', ['forfait_id' => $forfait->id]) }}">Réservez</a>
-                    @endif
+
+                    <div class="prix">
+                        <p>à partir de</p>
+                        <p class="tarif">99$</p>
+                    </div>
+
+                    <div class="btn-reserver">
+                        @if (auth()->check())
+                            <a href="{{ route('user.panier', ['forfait_id' => $forfait->id]) }}">Réservez</a>
+                        @else
+                            <a href="{{ route('user_connexion.create', ['forfait_id' => $forfait->id]) }}">Réservez</a>
+                        @endif
+                    </div>
                 </div>
             @endforeach
         </div>
     </div>
     <x-footer />
-
 </x-layout>
